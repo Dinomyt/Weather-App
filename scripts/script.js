182c77edfad78ecf46ac24a39c67f34604aa38fe
Dinomyt <nengithub@gmail.com>
2024-03-25 11:11:36 -0700

    +added font weights to accurately depict figma design

--- scripts/script.js
+++ scripts/script.js
@@ -33,13 +33,13 @@ async function getWeather() {
     const formattedDate = getFormattedDate(data.dt);
     const dayOfWeek = getDayOfWeek(data.dt);
     currentCityDisplay.innerHTML = data.name;
-    currentTempDisplay.innerHTML = `${Math.round(data.main.temp)}&#8457;`;
+    currentTempDisplay.innerHTML = `${Math.round(data.main.temp)}&deg;`;
     currentWeatherDisplay.innerHTML = data.weather[0].description;
     todaysDate.innerHTML = formattedDate;
     currentDay.innerHTML = dayOfWeek;
-    maxTemp.innerHTML = `${Math.round(data.main.temp_max)}&#8457`;
-    minTemp.innerHTML = `${Math.round(data.main.temp_min)}&#8457;`;
-    feelsLikeTemp.innerHTML = `${Math.round(data.main.feels_like)}&#8457;`;
+    maxTemp.innerHTML = `${Math.round(data.main.temp_max)}&deg`;
+    minTemp.innerHTML = `${Math.round(data.main.temp_min)}&deg;`;
+    feelsLikeTemp.innerHTML = `${Math.round(data.main.feels_like)}&deg;`;
     humidity.innerHTML = `${data.main.humidity}%`
     updateWeatherImage(data.weather[0].icon ,'currentWeatherImg')
   } catch (error) {
@@ -55,27 +55,27 @@ async function getForecast() {
     // Display 5-day forecast
     document.getElementById('dayOne').textContent = getDayOfWeek(data.list[1].dt);
     document.getElementById('dayOneDate').textContent = getFormattedDate(data.list[1].dt);
-    document.getElementById('dayOneTemp').innerHTML = `${Math.round(data.list[1].temp.day)} &#8457;`;
+    document.getElementById('dayOneTemp').innerHTML = `${Math.round(data.list[1].temp.day)}&deg;`;
     updateWeatherImage(data.list[1].weather[0].icon, 'dayOneImg');
 
     document.getElementById('dayTwo').textContent = getDayOfWeek(data.list[2].dt);
     document.getElementById('dayTwoDate').textContent = getFormattedDate(data.list[2].dt);
-    document.getElementById('dayTwoTemp').innerHTML = `${Math.round(data.list[2].temp.day)} &#8457;`;
+    document.getElementById('dayTwoTemp').innerHTML = `${Math.round(data.list[2].temp.day)}&deg;`;
     updateWeatherImage(data.list[2].weather[0].icon, 'dayTwoImg');
 
     document.getElementById('dayThree').textContent = getDayOfWeek(data.list[3].dt);
     document.getElementById('dayThreeDate').textContent = getFormattedDate(data.list[3].dt);
-    document.getElementById('dayThreeTemp').innerHTML = `${Math.round(data.list[3].temp.day)} &#8457;`;
+    document.getElementById('dayThreeTemp').innerHTML = `${Math.round(data.list[3].temp.day)}&deg;`;
     updateWeatherImage(data.list[3].weather[0].icon, 'dayThreeImg');
 
     document.getElementById('dayFour').textContent = getDayOfWeek(data.list[4].dt);
     document.getElementById('dayFourDate').textContent = getFormattedDate(data.list[4].dt);
-    document.getElementById('dayFourTemp').innerHTML = `${Math.round(data.list[4].temp.day)} &#8457;`;
+    document.getElementById('dayFourTemp').innerHTML = `${Math.round(data.list[4].temp.day)}&deg;`;
     updateWeatherImage(data.list[4].weather[0].icon, 'dayFourImg');
 
     document.getElementById('dayFive').textContent = getDayOfWeek(data.list[5].dt);
     document.getElementById('dayFiveDate').textContent = getFormattedDate(data.list[5].dt);
-    document.getElementById('dayFiveTemp').innerHTML = `${Math.round(data.list[5].temp.day)} &#8457;`;
+    document.getElementById('dayFiveTemp').innerHTML = `${Math.round(data.list[5].temp.day)}&deg;`;
     updateWeatherImage(data.list[5].weather[0].icon, 'dayFiveImg');
 
   } catch (error) {
